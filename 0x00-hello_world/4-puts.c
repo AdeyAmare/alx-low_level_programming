76fd729d78d27e778a3ff2765840ba99d6f36253
AdeyAmare <amareadey@gmail.com>
2022-03-10 08:46:32 -0800

    Task 4 Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 /**
  * main - Entry point prints the statement in quotes
- *  returns  a zero if exit is successful, non-zero otherwise
+ *
+ *  Return:  a zero if exit is successful, non-zero otherwise
  */
-
 int main(){
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
