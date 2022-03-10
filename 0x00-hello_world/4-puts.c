d156bd5f10268db1e5988179dcff69bca0444823
AdeyAmare <amareadey@gmail.com>
2022-03-10 08:49:52 -0800

    Task 4 Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  *
  *  Return:  a zero if exit is successful, non-zero otherwise
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
 
