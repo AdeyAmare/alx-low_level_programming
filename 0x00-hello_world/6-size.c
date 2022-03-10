0119324fc168175414aa77cf8c61db0ebf349b72
AdeyAmare <amareadey@gmail.com>
2022-03-10 09:16:44 -0800

    Task 6 Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point prints the size of types on a computer
  *
- * return: zero if success output non zero otherwise
+ * Return: zero if success output non zero otherwise
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
 
