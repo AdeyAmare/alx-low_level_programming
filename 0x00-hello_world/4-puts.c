de4c5baedf2fea76ac6f2b6301d13c8b28f7289b
AdeyAmare <amareadey@gmail.com>
2022-03-10 08:38:46 -0800

    Task 4 Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point prints "Programming is like building a multilingual puzzle
+ * returns 0 if exit is successful, non-zero otherwise
+ */
+
 int main(){
 	puts(\"Programming is like building a multilingual puzzle");
-	return(0)
+	return(0);
+}
+
