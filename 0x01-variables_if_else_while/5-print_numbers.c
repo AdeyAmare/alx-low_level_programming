76b6996aba43ff7134209128f4ea2dee52ea86e3
AdeyAmare <amareadey@gmail.com>
2022-03-11 06:31:04 -0800

    Task 5 Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers of base 10
+ * Return: 0 (Always Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
