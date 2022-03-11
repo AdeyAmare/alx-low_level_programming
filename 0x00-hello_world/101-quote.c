d76d57766fcfd50dc3d37f0125714724b71fccd8
AdeyAmare <amareadey@gmail.com>
2022-03-11 08:19:09 -0800

    Add advanced task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 39, 1, stderr);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 39, 1);
 	return (1);
 
 }
