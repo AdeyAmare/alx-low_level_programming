e870418eb7d01f7b1955877a3a5b29e1ba5db87a
AdeyAmare <amareadey@gmail.com>
2022-03-16 11:59:31 -0700

    Advanced task commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, sum;
 
-	for (i = 1; i < 1024; a++)
+	for (i = 1; i < 1024; i++)
 	{
-		if ((a % 3) == 0 || (a % 5) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
 	printf("%d\n", sum);
