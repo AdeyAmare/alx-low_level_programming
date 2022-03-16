45669135a8abb52f7f75fd3c6b654b72ad871567
AdeyAmare <amareadey@gmail.com>
2022-03-16 11:02:12 -0700

    Advanced task commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	long int i, j, next;
+	long int i, j, k, next;
 
 	j = 1;
 
 	k = 2;
 
-	for (i = 1; i < = 50; i++)
+	for (i = 1; i <= 50; i++)
 	{
 		if (j != 20365011074)
 		{
