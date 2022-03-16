71f426703317df7ed9bf0bbdfdd5dbb054511087
AdeyAmare <amareadey@gmail.com>
2022-03-16 09:08:56 -0700

    Add task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,8 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i < 24; i++)
 	{
@@ -16,7 +17,7 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
-			_putchar('\n');	
+			_putchar('\n');
 		}
 	}
 }
