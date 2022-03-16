a02d4425c08beb1a5d99f52ff54c074822f41b2c
AdeyAmare <amareadey@gmail.com>
2022-03-16 00:32:46 -0700

    Functions commit 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * main - prints _putchar followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write("_putchar");
+	printf("_putchar");
 	return (0);
 }
