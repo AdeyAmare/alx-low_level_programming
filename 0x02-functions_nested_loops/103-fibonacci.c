d3512020242a03c519a9bc8c66560818345a8e7e
AdeyAmare <amareadey@gmail.com>
2022-03-16 12:12:55 -0700

    Add Advanced task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonaccci numbers that dont
+ * exceed 4000000
+ * Return: 0 (Always Success)
+ */
+int main(void)
+{
+	long int sum, i, j, k;
+	sum = 0;
+	i = 1;
+	j = 2;
+
+	while (k < 4000000)
+	{
+		k = i + j;
+		if (k % 2 == 0)
+			sum += k;
+		i = j;
+		j = k;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
