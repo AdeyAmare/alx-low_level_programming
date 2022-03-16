1e38861d09196f5cc07046fb7bc0f2478e2f1562
AdeyAmare <amareadey@gmail.com>
2022-03-16 00:45:02 -0700

    Add Task 1

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes a character to the stdout
+ * @c: The character to print
+ *
+ * Return : returns a character output
+ */
+int _putchar(char c)
+{
+	return (putchar(&c));
+}
