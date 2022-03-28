3256a27099c5b5f3b928a1712b22102963d30ed2
AdeyAmare <amareadey@gmail.com>
2022-03-28 10:00:50 -0700

    Add task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,22 +11,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s);
 		}
-		i++;
-	}
-	if (s[i] == c)
-	{
-		return (s);
-	}
-	if (s[i] == 0)
-	{
-		return (NULL);
 	}
 }
