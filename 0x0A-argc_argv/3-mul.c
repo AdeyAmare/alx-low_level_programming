419dd2ddd4248f0e42fc5d83f47c6d71d9ebb69d
AdeyAmare <amareadey@gmail.com>
2022-04-04 09:02:39 -0700

    Add task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,21 +6,13 @@
  * @argv: array of arguments
  * Return: 0 (Success) 1(Error)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-	int product, num, num1;
-
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num = atoi(argv[1]);
-	num1 = atoi(argv[2]);
-	product = num * num1;
-
-	printf("%d\n", product);
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
