cf0ba18a63a7d420768f4646a2f6d4879bb40e61
AdeyAmare <amareadey@gmail.com>
2022-04-05 05:43:34 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,12 +5,13 @@
  * @s: string to be encoded
  * Return: the encoded string
  */
-char *rot13(char*s)
+char *rot13(char *s)
 {
 	int i, j;
 
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
